9c4b80f214347906567486497d7c3a36759b0c8e
Sana-Faisal86 <164844571+Sana-Faisal86@users.noreply.github.com>
2024-04-23 03:43:26 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,10 @@
+// Area of Circle :
+// ====> WE USE FUNCTION METHOD AND DEFINE AREA OF CIRCLE : <=======
+
+function areaofCircle(r: number) {
+  let areaofCircle = Math.PI * r ** 2; // <=== we use Math formula :
+  return areaofCircle;
+}
+
+let radius = areaofCircle(4); // <==== change radius and change result:
+console.log(" \n\tArea of Circle is : ", radius);
